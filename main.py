de949eb9b6fec72d44f1f5db6fb593296675112b
Kopylov-Andrey <66756782+Kopylov-Andrey@users.noreply.github.com>
2023-12-29 23:18:08 +0300

    Koch's snowflake

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import turtle
 
-def mandelbrot(c, max_iter):
-   z = 0
-   n = 0
-   while abs(z) <= 2 and n < max_iter:
-       z = z**2 + c
-       n += 1
-   if n == max_iter:
-       return max_iter
-   return n + 1 - np.log(np.log2(abs(z)))
+def koch_snowflake(turtle, order, size):
+   if order == 0:
+       turtle.forward(size)
+   else:
+       for angle in [60, -120, 60, 0]:
+           koch_snowflake(turtle, order - 1, size / 3)
+           turtle.left(angle)
 
-def mandelbrot_set(width, height, x_min, x_max, y_min, y_max, max_iter):
-   x, y = np.linspace(x_min, x_max, width), np.linspace(y_min, y_max, height)
-   X, Y = np.meshgrid(x, y)
-   Z = X + 1j * Y
-   fractal = np.zeros(Z.shape)
+def main():
+   turtle.speed(0)
+   turtle.hideturtle()
+   turtle.bgcolor("white")
+   turtle.color("blue")
 
-   for i in range(width):
-       for j in range(height):
-           fractal[i, j] = mandelbrot(Z[i, j], max_iter)
+   order = 5
+   size = 300
 
-   return fractal
+   turtle.penup()
+   turtle.goto(-size / 2, -size / 2)
+   turtle.pendown()
 
-def plot_fractal(fractal):
-   plt.imshow(fractal, cmap='viridis', extent=(x_min, x_max, y_min, y_max))
-   plt.colorbar()
-   plt.show()
+   koch_snowflake(turtle, order, size)
 
-# Параметры для построения фрактала Мандельброта
-width, height = 800, 800
-x_min, x_max = -2, 2
-y_min, y_max = -2, 2
-max_iter = 500
+   turtle.done()
 
-fractal = mandelbrot_set(width, height, x_min, x_max, y_min, y_max, max_iter)
-plot_fractal(fractal)
+if __name__ == "__main__":
+   main()
